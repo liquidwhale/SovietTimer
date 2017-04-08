62a7c6411a39f6f2a3edca6edd37a12f2ddae3f6
Arseniy Rozov <rseek@users.noreply.github.com>
2017-04-08 15:03:56 +0700

    pass time to TimerActivity

--- app/src/main/java/cloud/akadem/soviettimer/MainActivity.java
+++ app/src/main/java/cloud/akadem/soviettimer/MainActivity.java
@@ -4,17 +4,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    public static final String EXTRA_TIME_PERIOD = "cloud.akadem.soviettimer.EXTRA_TIME_PERIOD";
+    public static final int timePeriod = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTimePeriodTextView();
     }
 
     public void startTimer(View view) {
         Intent intent = new Intent(this, TimerActivity.class);
+        intent.putExtra(EXTRA_TIME_PERIOD, Integer.toString(timePeriod));
         startActivity(intent);
+    }
+
+    public void initTimePeriodTextView() {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(Integer.toString(timePeriod));
     }
 }
