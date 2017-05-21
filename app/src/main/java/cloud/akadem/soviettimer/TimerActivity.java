7244a36facc9826456b0a22735b70d4b8bfa3b53
Arseniy Rozov <rseek@users.noreply.github.com>
2017-05-21 19:22:43 +0700

    add CountDownTimer to TimerActivity

--- app/src/main/java/cloud/akadem/soviettimer/TimerActivity.java
+++ app/src/main/java/cloud/akadem/soviettimer/TimerActivity.java
@@ -1,13 +1,45 @@
 package cloud.akadem.soviettimer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class TimerActivity extends Activity {
+    CountDownTimer timer;
+    TextView countDownTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        countDownTextView = (TextView) findViewById(R.id.textView2);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String timePeriod = intent.getStringExtra(MainActivity.EXTRA_TIME_PERIOD);
+
+        countDownTextView.setText(timePeriod);
+
+        timer = new CountDownTimer(Integer.parseInt(timePeriod) * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int n = (int) millisUntilFinished;
+                countDownTextView.setText("" + n);
+            }
+
+            @Override
+            public void onFinish() {
+                countDownTextView.setText("Done");
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        timer.start();
     }
 }
